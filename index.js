79ae9e10f855b4f4965366cb7afc0156e5803e0d
jattiemyaseen <yaseenjattiem0000@gmail.com>
2022-08-24 11:18:00 +0200

    Updates

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.json()); // Enable the server to handle JSON requests
 app.use(cors()); // Dont let local development give errors
 
 app.get("/", (req, res) => {
-  res.json({ msg: "Welcome to Gaming Hardware" });
+  res.status(200).json({ msg: "Welcome to Gaming Hardware" });
 });
 
 const productsRoute = require("./routes/products");
